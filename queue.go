81f23bfb71bc976a1b0b937789bf8060dc38d706
Miles McLean <Mills00013@gmail.com>
2020-07-26 13:11:44 -0500

    Added detailed comments for each action

--- queue.go
+++ queue.go
@@ -19,32 +19,40 @@ import (
 	"cloud.google.com/go/pubsub"
 )
 
+// Variables used for Slack validation that will not change.
 const (
 	version                     = "v0"
 	slackRequestTimestampHeader = "X-Slack-Request-Timestamp"
 	slackSignatureHeader        = "X-Slack-Signature"
 )
 
+// Variables used for the GCP Pub/Sub connection.
 var (
 	projectID string
 	topicName string
 )
 
+// Variables used for Slack validation.
 var (
 	slackSigSecret string
 	slackChannelID string
 )
 
+// Struct for the message to be sent to the GCP Pub/Sub engine.
 type queueMessage struct {
 	Query       string `json:"query"`
 	ResponseUrl string `json:"response_url"`
 }
 
+// Struct for the message to be sent back to Slack after the
+// initial contact.
 type queueResponse struct {
 	ResponseType string `json:"response_type"`
 	Text         string `json:"text"`
 }
 
+// init() runs at the beginning of our GCF and sets the variables needed
+// for the queue process from the env variables set in the GCF.
 func init() {
 	projectID = os.Getenv("GCP_PROJECT_ID")
 	topicName = os.Getenv("GCP_TOPIC_NAME")
@@ -53,6 +61,9 @@ func init() {
 	slackChannelID = os.Getenv("SLACK_CHANNEL_ID")
 }
 
+// main() does not run in GCF. It is left here strictly for testing
+// responses locally. To compile locally, change package name
+// to "main" and run `go build`.
 func main() {
 	http.HandleFunc("/response", LocalResponse)
 	http.HandleFunc("/queue", Queue)
@@ -63,24 +74,38 @@ func main() {
 	}
 }
 
+// Main entry point for GCF anerbot-queue function. An HTTP request
+// to the cloud function is sent directly to Queue() and the rest
+// of the process launches from this point.
 func Queue(w http.ResponseWriter, r *http.Request) {
+	// Grab the raw body in bytes from the original request and
+	// create a readable buffer for other functions to use.
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatalf("Couldn't read request body: %v", err)
 	}
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
+	// Check if the method of the request was a "POST". Messages
+	// from Slack should not come in any other method.
 	if r.Method != "POST" {
 		http.Error(w, "Only POST requests are accepted", 405)
 	}
 
+	// Parse the body of the POST request and gather the data
+	// into a new field on the request called Form (accessed
+	// via r.Form)
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Couldn't parse form", 400)
 		log.Fatalf("ParseForm: %v", err)
 	}
 
-	// Reset r.Body as ParseForm depletes it by reading the io.ReadCloser.
+	// Reset r.Body field as ParseForm depletes it by reading
+	// the io.ReadCloser.
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	// Validate that our request is legitimate and actually came
+	// from Snyk's Slack.
 	ok, err := verifyWebHook(r, slackSigSecret)
 	if err != nil {
 		log.Fatalf("verifyWebhook: %v", err)
@@ -89,10 +114,14 @@ func Queue(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("signatures did not match.")
 	}
 
+	// Validate that the entire form is actually present.
 	if len(r.Form["text"]) == 0 {
 		log.Fatalf("empty text in form")
 	}
 
+	// Validate the query itself from the form. Check for
+	// an empty query and omit the word "search" if present
+	// to maintain backwards compatibility with Anerbot 1.0.
 	queryText := r.Form["text"][0]
 	if queryText == "" {
 		http.Error(w, "Unable to search for an empty string", 400)
@@ -101,23 +130,31 @@ func Queue(w http.ResponseWriter, r *http.Request) {
 		queryText = strings.TrimPrefix(queryText, "search ")
 	}
 
+	// Prepare the message to the queue made up of two
+	// components: the query from the user, and the URL that
+	// Slack will be listening on for additional messages.
 	message := queueMessage{
 		Query:       queryText,
 		ResponseUrl: r.Form["response_url"][0],
 	}
 
+	// Send the message (publish) to the GCP Pub/Sub engine.
+	// As soon as a message is received, the GCF anerbot-response
+	// function is kicked off and operates on the message.
 	err = publishMessage(message)
 	if err != nil {
 		log.Fatalf("unable to publish message: %v", err)
 	}
 
+	// Prepare the message to be immediately sent back to Slack
+	// in an attempt to beat their three second timeout.
 	responseText := fmt.Sprintf(`Hang tight - gathering results for "%s".`, queryText)
-
 	res := queueResponse{
 		ResponseType: "ephemeral",
 		Text:         responseText,
 	}
 
+	// Marshal our response struct into JSON and send it back to Slack.
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	err = json.NewEncoder(w).Encode(res)
@@ -126,23 +163,35 @@ func Queue(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Function to send our message to the GCP Pub/Sub Engine.
 func publishMessage(message queueMessage) error {
+	// Marshal our message struct into JSON.
 	m, err := json.Marshal(message)
 	if err != nil {
 		return fmt.Errorf("unable to convert message to json: %v", err)
 	}
 
+	// Create a new Pub/Sub client that will allow further operations.
+	// The client automatically pulls authentication credentials
+	// from the Service Account running to anerbot-queue Cloud
+	// Function, anerbot. If this function is being run locally for
+	// testing purposes, the `GOOGLE_APPLICATION_CREDENTIALS` env
+	// variable must be set and pointing to a GCP JSON credential
+	// file for the anerbot Service Account.
 	ctx := context.Background()
 	client, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
 		return fmt.Errorf("unable to create pubsub client: %v", err)
 	}
 
+	// Set the Topic to be used, usually "anerbot" but configurable
+	// in the GCF environment variables, and publish the message.
 	t := client.Topic(topicName)
 	result := t.Publish(ctx, &pubsub.Message{
 		Data: m,
 	})
 
+	// Ensure the publishing was successful. Throw away the result.
 	_, err = result.Get(ctx)
 	if err != nil {
 		return fmt.Errorf("unable to get published result: %v", err)
@@ -151,23 +200,29 @@ func publishMessage(message queueMessage) error {
 	return nil
 }
 
+// Function to validate that the request we received was actually from Slack.
 func verifyWebHook(r *http.Request, slackSigningSecret string) (bool, error) {
+	// Set basic control data  from the request itself.
 	timeStamp := r.Header.Get(slackRequestTimestampHeader)
 	slackSignature := r.Header.Get(slackSignatureHeader)
 
+	// Convert the timestamp into an integer for comparing.
 	t, err := strconv.ParseInt(timeStamp, 10, 64)
 	if err != nil {
 		return false, fmt.Errorf("strconv.ParseInt(%s): %v", timeStamp, err)
 	}
 
+	// Validate that the time this message was sent was within the last five minutes.
 	if ageOk, age := checkTimestamp(t); !ageOk {
 		return false, fmt.Errorf("checkTimestamp(%v): %v %v", t, ageOk, age)
 	}
 
+	// Verify that the headers actually contained the needed controls.
 	if timeStamp == "" || slackSignature == "" {
 		return false, fmt.Errorf("either timeStamp or signature headers were blank")
 	}
 
+	// Generate a slice of bytes representing the body for hashing.
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return false, fmt.Errorf("ioutil.ReadAll(%v): %v", r.Body, err)
@@ -176,27 +231,36 @@ func verifyWebHook(r *http.Request, slackSigningSecret string) (bool, error) {
 	// Reset the body so other calls won't fail.
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
+	// Create the string used to validate the signature. The string is
+	// based on the Slack version (which is always "v0"), the timestamp,
+	// and the body itself.
 	baseString := fmt.Sprintf("%s:%s:%s", version, timeStamp, body)
 
+	// Generate the signature of this request based on all the parts and the
+	// original signing secret from Slack.
 	signature := getSignature([]byte(baseString), []byte(slackSigningSecret))
 
+	// Drop the "v0=" off the front of the signature since the computed
+	// one will not have it. Convert the trimmed hex string into bytes.
 	trimmed := strings.TrimPrefix(slackSignature, fmt.Sprintf("%s=", version))
 	signatureInHeader, err := hex.DecodeString(trimmed)
-
 	if err != nil {
 		return false, fmt.Errorf("hex.DecodeString(%v): %v", trimmed, err)
 	}
 
+	// Compare the two values and return true if they are a match.
 	return hmac.Equal(signature, signatureInHeader), nil
 }
 
-// Arbitrarily trusting requests time stamped less than 5 minutes ago.
+// Function to validate the time of the request being set.
 func checkTimestamp(timeStamp int64) (bool, time.Duration) {
 	t := time.Since(time.Unix(timeStamp, 0))
 
+	// Arbitrarily trusting messages sent within the last five minutes.
 	return t.Minutes() <= 5, t
 }
 
+// Function to generate a checksum used to compare the secrets.
 func getSignature(base []byte, secret []byte) []byte {
 	h := hmac.New(sha256.New, secret)
 	h.Write(base)
--- response.go
+++ response.go
@@ -14,6 +14,7 @@ import (
 	"github.com/smfsh/airtable-go"
 )
 
+// Variables used for the Airtable connection.
 var (
 	airtableAPIKey  string
 	airtableBaseID  string
@@ -21,6 +22,7 @@ var (
 	airtableViewID  string
 )
 
+// Struct to contain each "feature" returned from an Airtable query.
 type feature struct {
 	AirtableID string `json:"id"`
 	Fields     struct {
@@ -34,6 +36,7 @@ type feature struct {
 	}
 }
 
+// Struct for the message to be sent to Slack.
 type slackResponse struct {
 	ReplaceOriginal bool         `json:"replace_original"`
 	ResponseType    string       `json:"response_type"`
@@ -41,6 +44,10 @@ type slackResponse struct {
 	Attachments     []attachment `json:"attachments,omitempty"`
 }
 
+// Struct for each attachment in the Slack message. Each of
+// these represents one unique "feature". Title is what will
+// normally be displayed to a user and fallback will be used
+// in the event that rich markdown cannot be rendered.
 type attachment struct {
 	Title     string            `json:"title"`
 	Fallback  string            `json:"fallback"`
@@ -48,15 +55,21 @@ type attachment struct {
 	Fields    []attachmentField `json:"fields"`
 }
 
+// Struct to represent the information printed to the requester
+// in Slack for each "feature". The title field should always
+// be blank and value will always contain markdown.
 type attachmentField struct {
 	Title string `json:"title"`
 	Value string `json:"value"`
 }
 
+// Struct for the message to be received from the GCP Pub/Sub engine.
 type PubSubMessage struct {
 	Data []byte `json:"data"`
 }
 
+// init() runs at the beginning of our GCF and sets the variables needed
+// for the response process from the env variables set in the GCF.
 func init() {
 	airtableAPIKey = os.Getenv("AIRTABLE_API_KEY")
 	airtableBaseID = os.Getenv("AIRTABLE_BASE_ID")
@@ -64,24 +77,35 @@ func init() {
 	airtableViewID = os.Getenv("AIRTABLE_VIEW_ID")
 }
 
+// Main entry point for GCF anerbot-response function. When a new message
+// is added to the anerbot Topic in Pub/Sub, this function is called and
+// the message is passed into it as an argument.
 func Response(ctx context.Context, m PubSubMessage) error {
+	// Unmarshal the JSON contained in the message. The message
+	// will contain the original search term and the Slack URL where
+	// the final results can be posted to.
 	var message queueMessage
 	err := json.Unmarshal(m.Data, &message)
 	if err != nil {
 		return fmt.Errorf("could not unmarshal message: %v", err)
 	}
 
+	// Perform the search in Airtable, passing in the original query term.
+	// Respond with a failure message if Airtable is unreachable for any reason.
 	atr, err := queryAirtable(message.Query)
 	if err != nil {
 		sendFailureMessage(message.ResponseUrl)
 		return fmt.Errorf("error querying Airtable: %v", err)
 	}
 
+	// Build the full response object to be sent back to Slack.
 	res, err := buildSlackResponse(atr)
 	if err != nil {
 		return fmt.Errorf("unable to build slack response: %v", err)
 	}
 
+	// Marshal the response object into JSON and prepare the request to be
+	// sent to the ResponseUrl that was in the original message.
 	body, err := json.Marshal(res)
 	if err != nil {
 		return fmt.Errorf("unable to convert slack message to JSON: %v", err)
@@ -92,6 +116,8 @@ func Response(ctx context.Context, m PubSubMessage) error {
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	// Perform the request (posting our message to Slack,) and
+	// close out the response body sent back.
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -101,11 +127,18 @@ func Response(ctx context.Context, m PubSubMessage) error {
 	return nil
 }
 
+// Function to send a message to Slack informing the user that the program
+// was unable to communicate with Slack.
 func sendFailureMessage(url string) {
+	// Prepare message to be sent to Slack.
 	message := slackResponse{
 		ResponseType: "ephemeral",
 		Text:         "Failed to fetch records from Airtable :sob:",
 	}
+
+	// Marshal the message into JSON and prepare the request to be sent
+	// to the URL passed into this function. This should always be the
+	// ResponseUrl field from the original message.
 	body, err := json.Marshal(message)
 	if err != nil {
 		log.Fatalf("unable to convert slack message to JSON: %v", err)
@@ -116,6 +149,8 @@ func sendFailureMessage(url string) {
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	// Perform the request (posting our message to Slack,) and
+	// close out the response body sent back.
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -124,40 +159,58 @@ func sendFailureMessage(url string) {
 	defer resp.Body.Close()
 }
 
+// Function utilized strictly for local testing of the response object
+// to be sent back to Slack. In order to use this function, change this
+// package name and the one in queue.go to "main" and run `go build`.
 func LocalResponse(w http.ResponseWriter, r *http.Request) {
+	// Grab the raw body in bytes from the original request and
+	// create a readable buffer for other functions to use.
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatalf("Couldn't read request body: %v", err)
 	}
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
+	// Check if the method of the request was a "POST". Messages
+	// from Slack should not come in any other method.
 	if r.Method != "POST" {
 		http.Error(w, "Only POST requests are accepted", 405)
 	}
 
+	// Parse the body of the POST request and gather the data
+	// into a new field on the request called Form (accessed
+	// via r.Form)
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Couldn't parse form", 400)
 		log.Fatalf("ParseForm: %v", err)
 	}
 
-	// Reset r.Body as ParseForm depletes it by reading the io.ReadCloser.
+	// Reset r.Body field as ParseForm depletes it by reading
+	// the io.ReadCloser.
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
+	// Validate the query itself from the form. Check for
+	// an empty query and omit the word "search" if present
+	// to maintain backwards compatibility with Anerbot 1.0.
 	queryText := r.Form["text"][0]
 	if strings.HasPrefix(queryText, "search") {
 		queryText = strings.TrimPrefix(queryText, "search ")
 	}
 
+	// Perform the search in Airtable, passing in the original query term.
+	// Respond with a failure message if Airtable is unreachable for any reason.
 	atr, err := queryAirtable(queryText)
 	if err != nil {
 		log.Fatalf("error querying Airtable: %v", err)
 	}
 
+	// Build the full response object to be sent back to Slack.
 	res, err := buildSlackResponse(atr)
 	if err != nil {
 		log.Fatalf("unable to build slack response: %v", err)
 	}
 
+	// Marshal our response struct into JSON and respond to the request.
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(w).Encode(res)
@@ -166,22 +219,36 @@ func LocalResponse(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Function to build the response to be sent to Slack. The slackResponse
+// object will contain all the data needed for Slack to display the message.
 func buildSlackResponse(f []feature) (*slackResponse, error) {
+	// Prepare the top level statement of our results which reports
+	// whether there were any results from Airtable or not by counting
+	// the slice of features (f) passed into the function.
 	var text string
 	if len(f) == 0 {
 		text = "No items found, try another search term"
 	} else {
 		text = fmt.Sprintf("Found %d items! Click on any result to learn more.", len(f))
 	}
+
+	// Initialize the response object with some default values.
 	res := &slackResponse{
 		ReplaceOriginal: true,
 		ResponseType:    "ephemeral",
 		Text:            text,
 		Attachments:     nil,
 	}
+
+	// Prepare an attachment object for each feature in the feature slice.
 	for _, v := range f {
+		// Generate a link to this specific feature in Airtable.
 		link := fmt.Sprintf("https://airtable.com/%s/%s/%s", airtableTableID, airtableViewID, v.AirtableID)
 
+		// Create a single string that represents each possible field from
+		// Airtable. Each part is concatenated to the previous part. Fields
+		// are visually separated in Slack via the inclusion of `\r\n` which
+		// represents a return and new line.
 		var value string
 		if v.Fields.Roadmap != "" {
 			value += fmt.Sprintf(":sparkles: *Roadmap:* %s\r\n", v.Fields.Roadmap)
@@ -202,8 +269,13 @@ func buildSlackResponse(f []feature) (*slackResponse, error) {
 			value += fmt.Sprintf(":books: *Documentation:* %s\r\n", v.Fields.Documentation)
 		}
 
+		// Create a fallback title to be used in the case that rich markdown
+		// isn't available in the Slack client. This will come out in the
+		// following format: "Name of Feature: https://url.to/feature/in/airtable"
 		fallback := fmt.Sprintf("%s: %s", v.Fields.Feature, link)
 
+		// Add all of our crafted items to fields of an attachment object.
+		// Add the attachment object to the attachments field of the response.
 		res.Attachments = append(res.Attachments, attachment{
 			Title:     v.Fields.Feature,
 			Fallback:  fallback,
@@ -217,17 +289,23 @@ func buildSlackResponse(f []feature) (*slackResponse, error) {
 		})
 	}
 
+	// Return the Slack response object.
 	return res, nil
 }
 
+// Function to query Airtable for a search term.
 func queryAirtable(query string) ([]feature, error) {
+	// Initiate an Airtable client that will allow further operations.
 	client, err := airtable.New(airtableAPIKey, airtableBaseID)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create new airtable client: %v", err)
 	}
 
+	// Convert our query to lowercase to gather the most results.
 	query = strings.ToLower(query)
 
+	// Create a slice of strings containing each of the fields
+	// that should be queried in Airtable.
 	var fields = []string{
 		"Feature",
 		"Roadmap",
@@ -238,15 +316,22 @@ func queryAirtable(query string) ([]feature, error) {
 		"Documentation",
 	}
 
+	// Create an empty slice of strings that will be filled with
+	// strings representing an Airtable-compatible query-statement.
+	// There will be one statement created for each of the fields
+	// in the fields slice.
 	var searchStatements []string
-
 	for _, v := range fields {
 		statement := fmt.Sprintf("SEARCH('%s', LOWER({%s})) > 0", query, v)
 		searchStatements = append(searchStatements, statement)
 	}
 
+	// Create a single string, formula, by combining each of the elements
+	// in the searchStatements slice, separated by a comma.
 	var formula = fmt.Sprintf("OR(%s)", strings.Join(searchStatements, ", "))
 
+	// Initialize and populate the listParams object that will be
+	// used by the Airtable client to create a result set.
 	listParams := airtable.ListParameters{
 		CellFormat:      "string",
 		Fields:          fields,
@@ -256,12 +341,15 @@ func queryAirtable(query string) ([]feature, error) {
 		View:            airtableViewID,
 	}
 
+	// Initialize an empty slice of features to contain our results.
 	var features []feature
 
+	// Populate the features variable with results from Airtable.
 	err = client.ListRecords(airtableTableID, &features, listParams)
 	if err != nil {
 		return nil, err
 	}
 
+	// Return the slice of features for further processing.
 	return features, nil
 }
